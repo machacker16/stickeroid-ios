9e56e6021ecafe0e6c28bb199082b2af311c2ca2
Vadym Sidorov <lafleurbox@gmail.com>
2017-08-25 17:29:57 +0300

    Implement messenger-like UI basic logic

--- Stickeroid/ViewController.swift
+++ Stickeroid/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var resourceView: UIImageView!
-
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     let requestInstance = STRequest()
     
     @IBAction func performResourceSearch(_ sender: UITextField) {
+        
         guard let searchQuery = sender.text else {
             print("Search query is nil.")
             return
@@ -34,6 +37,16 @@ class ViewController: UIViewController {
             }
         }
     }
-
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y: 250), animated: true)
+        //scrollView.contentInset = UIEdgeInsetsMake(0, 0, 250, 0)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        //scrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+    }
+    
 }
 
